d52f2ea11dcb8c863df7e16ca2c2e0eca42a6e37
myatkyawthu <>
2023-11-01 16:40:37 +0630

    api integration

--- src/app/modules/car/modules/advanceSearch/components/AdvanceSearchForm.vue
+++ src/app/modules/car/modules/advanceSearch/components/AdvanceSearchForm.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>Advance search form</h1>
 </template>
+
+
+<script setup lang="ts">
+import  AdvanceSearchController from 'advanceSearch@/api/AdvanceSearchController'
+let advanceSearchController = AdvanceSearchController();
+</script>
