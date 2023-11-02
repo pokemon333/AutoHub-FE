ddc88d94a44c6df158fdeef9675f6847fe27bbe8
myatkyawthu <>
2023-11-02 16:34:00 +0630

    ui for advance search

--- src/app/modules/car/core/pages/CarList.vue
+++ src/app/modules/car/core/pages/CarList.vue
@@ -1,102 +1,66 @@
 <template>
-    <div class="w-full h-[88%] bg-primary-50/10" :class="advanceFileter ? 'flex':''">
+    <div class="w-full bg-primary-50/10 h-[88%]   " :class="advanceFileter ? 'flex':''">
         <!-- advance filter form -->
-        <div class=" p-10 bg-primary-50  max-lg:z-40 max-lg:h-screen max-lg:fixed" v-if="advanceFileter" :class="advanceFileter ? ' lg:w-6/12 w-screen order-2 ':'order-1 w-1/4'">
+        <div class=" lg:pr-1  lg:min-h-full h-fit 	 	 lg:pl-2 pl-2 pr-2 pt-8 min:h-fit sticky top-[12%]  bg-primary-50/70  max-lg:z-40 max-lg:min-h-screen max-lg:fixed"  :class="advanceFileter ? ' lg:w-6/12  w-screen order-2 block ':'order-1 w-1/4 hidden'">
             <div class="flex justify-between  w-full  h-10">
-                <h1 class="text-secondary-700 text-[25px]">Explore Specific</h1>
+                <h1 class="lg:ml-14 text-secondary-700 text-[25px]">Explore Specific</h1>
                 <div @click="toggleSideBar" class="flex cursor-pointer justify-center  pt-2 border-red-800  w-10 h-10  border  rounded-full items-center " :class="advanceFileter ?  'bg-secondary-700 ':''">
                         <advanceFilter :class="advanceFileter ?  'fill-gray-100':'fill-secondary-700'"/>
                 </div> 
             </div>
-            <div class=" flex flex-col space-y-10  w-full h-auto">
-                <div class="relative w-full mt-10">
-                    <label for="type" class="absolute px-2 -top-2 left-3 bg-primary-50 text-xs">Search Type</label>
-                    <select  name="type" class="w-full border bg-primary-50  border-primary-500  peer h-12 rounded-md px-3">
-                        <option value="dealer" class="bg-primary-100" >Dealer</option>
-                        <option value="brand" class="bg-primary-100">Brand</option>
-                    </select>
-                </div>
-                <div class="relative w-full mt-10">
-                    <label for="type" class="absolute px-2 -top-2 left-3 bg-primary-50 text-xs">Search Type</label>
-                    <select  name="type" class="w-full border bg-primary-50  border-primary-500  peer h-12 rounded-md px-3">
-                        <option value="dealer" class="bg-primary-100" >Dealer</option>
-                        <option value="brand" class="bg-primary-100">Brand</option>
-                    </select>
-                </div>
-
-                <div class="relative w-full mt-10">
-                    <label for="type" class="absolute px-2 -top-2 left-3 bg-primary-50 text-xs">Search Type</label>
-                    <select  name="type" class="w-full border bg-primary-50  border-gray-500  peer h-12 rounded-md px-3">
-                        <option value="dealer" class="bg-gray-100" >Dealer</option>
-                        <option value="brand" class="bg-gray-100">Brand</option>
-                    </select>
-                </div>
-
-                <div class="relative w-full mt-10">
-                    <label for="type" class="absolute px-2 -top-2 left-3 bg-primary-50 text-xs">Search Type</label>
-                    <select  name="type" class="w-full border bg-primary-50  border-gray-500  peer h-12 rounded-md px-3">
-                        <option value="dealer" class="bg-gray-100" >Dealer</option>
-                        <option value="brand" class="bg-gray-100">Brand</option>
-                    </select>
-                </div>
-                <div class="flex justify-center">
-                    <button class="lg:w-5/12 bg-red-500 rounded-md text-white py-2 w-full">
-                        Submit
-                    </button>
-                </div>
+            <div class="w-full h-auto">
+                <AdvanceSearchForm/>
             </div>
         </div>
         <!-- carlist part -->
-        <div class="w-full   pt-4 " :class="advanceFileter ? 'order-1':'order-2'">    
-            <div class="lg:pl-24 flex flex-wrap space-y-1 justify-between ">
-                <div class="lg:order-1  lg:pb-0 lg:px-0  pb-2 px-2 flex flex-col justify-between "  :class="advanceFileter ?  'lg:w-5/12':'lg:w-[25%]'"> 
-                    <!-- <div class="  flex  justify-center">
-                        <back  class="cursor-pointer"/>
-                    </div>    -->
-                    <div>
-                        <h1 class="lg:text-[24px] md:text-[3vw] text-[5vw] font-[500]">Search Cars In Myanmar</h1>
-                        <h1 class="lg:text-[16px] md:text-[2vw] text-[4vw] font-[500]">{{ count }} {{ count == '1' ? 'car': 'cars' }} listings found in AutoHub</h1>
+            <div class="w-full h-fit  pt-4 " :class="advanceFileter ? 'order-1':'order-2'">    
+                <div class="lg:pl-24 flex flex-wrap space-y-1 justify-between ">
+                    <div class="lg:order-1  lg:pb-0 lg:px-0  pb-2 px-2 flex flex-col justify-between "  :class="advanceFileter ?  'lg:w-5/12':'lg:w-[25%]'"> 
+                       
+                        <div>
+                            <h1 class="lg:text-[24px] md:text-[3vw] text-[5vw] font-[500]">Search Cars In Myanmar</h1>
+                            <h1 class="lg:text-[16px] md:text-[2vw] text-[4vw] font-[500]">{{ count }} {{ count == '1' ? 'car': 'cars' }} listings found in AutoHub</h1>
+                        </div>
+                </div>
+                <div class="lg:order-3 lg:none flex ml-4 justify-end items-center px-3" v-if="!advanceFileter">
+                    <div  @click="toggleSideBar" class="flex cursor-pointer   justify-center   border-red-800  w-10 h-10  border  rounded-full items-center " :class="advanceFileter ?  'bg-secondary-700 ':''">
+                        <advanceFilter :class="advanceFileter ?  'fill-gray-100':'fill-secondary-700'"/>
                     </div>
-               </div>
-               <div class="lg:order-3 lg:none flex ml-4 justify-end items-center px-3" v-if="!advanceFileter">
-                   <div  @click="toggleSideBar" class="flex cursor-pointer   justify-center   border-red-800  w-10 h-10  border  rounded-full items-center " :class="advanceFileter ?  'bg-secondary-700 ':''">
-                       <advanceFilter :class="advanceFileter ?  'fill-gray-100':'fill-secondary-700'"/>
-                   </div>
-               </div> 
-               <div  class="lg:order-2 lg:px-0  lg:ml-[70px] px-2 w-full " :class="advanceFileter ?  'lg:w-[46%] lg:mr-4 ':'lg:w-[60%] '" >
-                    <input v-model="keyword" @keyup="overallSearch" type="text" class="h-16 border border-secondary-400 rounded-md px-3 w-full" placeholder="Overall Search ( Name or Dealer )">
+                </div> 
+                <div  class="lg:order-2 lg:px-0  lg:ml-[70px] px-2 w-full " :class="advanceFileter ?  'lg:w-[46%] lg:mr-4 ':'lg:w-[60%] '" >
+                        <input v-model="keyword" @keyup="overallSearch" type="text" class="h-16 border border-secondary-400 rounded-md px-3 w-full" placeholder="Overall Search ( Name or Dealer )">
+                    </div>
+                </div>
+                <div class="w-full h-[88%] mt-8 flex  justify-center" :class="advanceFileter ?  'lg:justify-end pr-2 ':'lg:justify-center'">
+                    <div class="lg:px-0 md:grid-cols-2 bg-primary-50/10  max-sm:grid-cols-1 grid gap-x-4 gap-y-4  px-2   overflow-y-auto  h-full scroll-auto " :class="advanceFileter? 'lg:w-[90%]  lg:grid-cols-2 ':'lg:w-[85%]  lg:grid-cols-3'"  >
+                        <CarCard 
+                        @car-detail="deatil(car.id)"
+                        class=""
+                        :car="car" 
+                        v-for="car in data"
+                        :key="car.id"
+                        />
+                    </div>
                 </div>
             </div>
-            <div class="w-full h-[88%]  flex justify-center" :class="advanceFileter ?  'lg:justify-end ':'lg:justify-center'">
-                <div class="lg:px-0 md:grid-cols-2 bg-primary-50/10  max-sm:grid-cols-1 grid gap-[4vw] px-2   overflow-y-auto  h-full scroll-auto " :class="advanceFileter? 'lg:w-[90%]  lg:grid-cols-2 ':'lg:w-[85%]  lg:grid-cols-3'"  >
-                    <CarCard 
-                    @car-detail="deatil(car.id)"
-                    class="mt-8"
-                    :car="car" 
-                    v-for="car in data"
-                    :key="car.id"
-                    />
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
 
 <script setup lang="ts">
 import { ref,onMounted, computed } from 'vue'
-// import { AdvanceSearchForm } from 'car@/core/services/getCarListCompoent';
 import { advanceFilter } from 'car@/core/services/getCarCardSvg';
 import CarCard from 'car@/core/components/CarCard.vue';
 import { useCarStore } from 'car@/core/stores/CarStore';
 import { useRouter } from 'vue-router';
+import { AdvanceSearchForm } from '../services/getCarCompoent';
 // import { back } from 'car@/core/services/getCarCardSvg'
 
 
 let advanceFileter = ref<boolean>(false)
 let router = useRouter();
 let cars = ref<any>([])
-let count = ref(0)
+let count = ref<number|string>(0)
 let keyword = ref('')
 let carStore = useCarStore()
 
@@ -112,8 +76,6 @@ let toggleSideBar = ()=>{
 }
 
 let deatil = (id : string) => {
-    console.log(id);
-    
     return router.push({name:"car-detail" , params : { id : id}})
 }
 
--- src/app/modules/car/modules/advanceSearch/components/AdvanceSearchForm.vue
+++ src/app/modules/car/modules/advanceSearch/components/AdvanceSearchForm.vue
@@ -1,9 +1,173 @@
 <template>
-    <h1>Advance search form</h1>
+    <div class="pt-4 pb-4 lg:px-14  ">
+        <!-- Search Input and Dropdown -->
+        <div class=" w-1/2">
+            <select  name="type" class="w-full border bg-primary-50/70 mt-2  border-gray-500   peer h-10 rounded-md px-3">
+                <option value="dealer" style="background-color:#BDBDBD;">Dealer</option>
+                <option value="brand" style="background-color:#BDBDBD;">Brand</option>
+            </select>
+        </div>
+
+          <!-- Brand -->
+        <div class="mt-4">
+            <label for="">Brand</label>
+            <div class="mt-2">
+                <button type="button" @click="()=>brandDropDown = !brandDropDown" class="flex items-center border border-gray-500  w-full p-2 text-base text-gray-900 transition duration-75 rounded-md group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Division</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                <div class=" py-2 space-y-2" v-if="brandDropDown">
+                    <div class="px-2 flex space-x-2" v-for="(brand,index) in brands" :key="index">
+                        <input type="checkbox" :value="brand?.name" v-model="checkedBrand[index]" @change="filterModel">
+                        <h1>{{ brand?.name }}</h1>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+          <!-- Model -->
+        <div class="mt-4">
+            <label for="">Model</label>
+           <div class="mt-2">
+                <button type="button" @click="()=>divisionDropDown = !divisionDropDown" class="flex items-center border border-gray-500  w-full p-2 text-base text-gray-900 transition duration-75 rounded-md group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Division</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                <div class=" py-2 space-y-2" v-if="divisionDropDown">
+                    <div class="px-2 flex space-x-2" v-for="division in divisions">
+                        <input type="checkbox" :value="division?.id">
+                        <h1>{{ division?.name }}</h1>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+          <!-- Year -->
+        <div class="mt-4">
+            <label for="">Year</label>
+           <div class="mt-2">
+                <button type="button" @click="()=>divisionDropDown = !divisionDropDown" class="flex items-center border border-gray-500  w-full p-2 text-base text-gray-900 transition duration-75 rounded-md group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Division</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                <div class=" py-2 space-y-2" v-if="divisionDropDown">
+                    <div class="px-2 flex space-x-2" v-for="division in divisions">
+                        <input type="checkbox" :value="division?.id">
+                        <h1>{{ division?.name }}</h1>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+        <!-- condition -->
+        <div class="mt-4">
+            <label for="">Condition</label>
+            <div class="mt-2 flex space-x-3">
+                <div class="flex space-x-1 ">
+                    <input type="checkbox" value="new">
+                    <h1>New</h1>
+                </div>
+                <div class="flex space-x-1 ">
+                    <input type="checkbox" value="used">
+                    <h1>Used</h1>
+                </div>
+            </div>
+        </div>
+
+       
+
+        <!-- price -->
+        <div class="mt-4">
+            <label for="">Price</label>
+            <div class="h-10 flex space-x-1 mt-2  items-center">
+                <div class="w-1/2 h-full">
+                    <input type="text" class="w-full h-full rounded-md border border-gray-500 px-2 bg-primary-50/70" placeholder="min">
+                </div>
+                <div>
+                    <h1>-</h1>
+                </div>
+                <div class="w-1/2 h-full">
+                    <input type="text" class="w-full h-full rounded-md border border-gray-500 px-2 bg-primary-50/70" placeholder="max">
+                </div>
+            </div>
+        </div>
+
+         <!-- division -->
+         <div class="mt-4">
+            <label for="">Division</label>
+           <div class="mt-2">
+                <button type="button" @click="()=>divisionDropDown = !divisionDropDown" class="flex items-center border border-gray-500  w-full p-2 text-base text-gray-900 transition duration-75 rounded-md group" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                    <span class="flex-1 ml-3 text-left whitespace-nowrap">Division</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                </button>
+                <div class=" py-2 space-y-2" v-if="divisionDropDown">
+                    <div class="px-2 flex space-x-2" v-for="division in divisions">
+                        <input type="checkbox" :value="division?.id">
+                        <h1>{{ division?.name }}</h1>
+                    </div>
+                </div>
+           </div>
+        </div>
+
+        <div class="mt-4 flex justify-center">
+            <button class="bg-red-500 py-1 px-4 rounded-md text-white">
+                Search  
+            </button>
+        </div>
+
+    </div>
 </template>
 
 
 <script setup lang="ts">
+
 import  AdvanceSearchController from 'advanceSearch@/api/AdvanceSearchController'
+import { onMounted  , ref } from 'vue';
+
 let advanceSearchController = AdvanceSearchController();
+let brands      =  ref([]);
+let models      =  ref([]);
+let years       =  ref([]);
+let dealers     =  ref([]);
+let divisions   =  ref([]);
+let divisionDropDown = ref(false);
+let brandDropDown = ref(false);
+let checkedBrand = ref(Array(brands.value.length).fill(''));
+// let ModelDropDown = ref(false);
+// let YearDropDown = ref(false);
+
+// let filteredModels = ref([]);
+// let filteredYears = ref([]);
+
+let filterModel = () => {
+    console.log(checkedBrand.value)
+}
+
+let getResource = async () => {
+    try {
+        const res = await advanceSearchController.getResources();
+        let data =  await res.data.data
+        brands.value    = await data.brands;
+        models.value    = await data.models;
+        years.value     = await data.years;
+        dealers.value   = await data.dealers;
+        divisions.value = await data.divisions;
+        console.log(brands.value);
+        
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+onMounted(()=>{
+        getResource();
+})
 </script>
