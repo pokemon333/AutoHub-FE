565bff71147f78e9d81b55348419f0f5f869636c
myatkyawthu <>
2024-01-08 09:02:29 +0630

    NearBy feature

--- src/app/modules/evse/services/getEvseSvg.ts
+++ src/app/modules/evse/services/getEvseSvg.ts
@@ -1,7 +1,7 @@
 import Back from 'asset@/icon/back.svg'
 import Address from 'asset@/icon/evse-address.svg'
 import Phone from 'asset@/icon/phone.svg'
-import FuelStation from 'asset@/icon/car.svg'
+import FuelStation from 'asset@/img/fuel-station.png'
 export {
     Back,
     Address,
